8a43d7d68213aebe41c89ec6d97d7243c267cd8d
gk <demar.demidov@yandex.ru>
2024-07-18 21:28:02 +0300

    test

--- AplicationContext/src/main/java/org/itstep/xml_scan/Application.java
+++ AplicationContext/src/main/java/org/itstep/xml_scan/Application.java
@@ -1,23 +1,19 @@
-package org.itstep.scan;
+package org.itstep.xml_scan;
 
-import org.itstep.xml_scan.Dog;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
+
 public class Application {
-    private static ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Application.class);
+    private static ApplicationContext applicationContext = new AnnotationConfigApplicationContext(org.itstep.scan.Application.class);
     public static void main(String[] args) {
         //вывести все экземпляры бинов
         for (String bean: applicationContext.getBeanDefinitionNames()) {
             System.out.println(bean);
 
-
             applicationContext.getBean(Room.class).sound();
-            System.out.println(applicationContext.containsBean("cat"));
+            System.out.println(applicationContext.containsBean("catDao"));
         }
     }
 } 
