c9dfb4d4b8ad550bfe57f2a5ba87a13353a7094d
gk <demar.demidov@yandex.ru>
2024-07-22 21:30:14 +0300

    test

--- AOP/src/main/java/org/itstep/traffic/TrafficController.java
+++ AOP/src/main/java/org/itstep/traffic/TrafficController.java
@@ -26,6 +26,8 @@ public class TrafficController {
     @AfterReturning(pointcut = "execution(public String org.itstep.traffic.TrafficLight.*(..))", returning = "result")
     public void logAfterReturning(JoinPoint joinPoint, Object result) {
         String name = joinPoint.getSignature().getName();
+        String value = (String)result;
+        System.out.println(value);
         if (name.equals("switchYellow"))
             driver.ready();
         else if (name.equals("switchGreen"))
